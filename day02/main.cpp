c0674456ce30b556d7d535eeeaa60026fdb9fecf
Andrea Rastelli <rastelliandrea87@gmail.com>
2017-12-07 21:56:28 +0000

    Update day2 part 2

--- day02/main.cpp
+++ day02/main.cpp
@@ -6,14 +6,26 @@
 #include <algorithm>
 
 
-auto line_difference_minmax(std::vector<std::string> line) {
-    std::min(line);
-    std::max(line);
+auto line_difference_minmax(std::vector<int> line) {
+    std::sort(line.begin(), line.end());
+
+    return line.at(line.size() - 1) - line.at(0);
 }
 
 
-auto spreadsheet_checksum() {
-    return "";
+auto evenly_divisible_value(std::vector<int> line) {
+    for (std::size_t idx1=0; idx1<line.size()-1; ++idx1) {
+        for (std::size_t idx2=1; idx2<line.size(); ++idx2) {
+            auto v1 = line.at(idx1);
+            auto v2 = line.at(idx2);
+
+            if ((v1 % v2 == 0) && (v1 / v2 != 1)) {
+                return v1 / v2;
+            }
+        }
+    }
+
+    return 0;
 }
 
 
@@ -23,17 +35,28 @@ int main() {
     auto input_file = std::ifstream("day02/input.txt");
 
     if (input_file.is_open()) {
+
+        auto count1 = 0;
+        auto count2 = 0;
+
         while(getline(input_file, line)) {
             auto ilineit = std::istringstream(line);
             auto num_list = std::vector<std::string>(
                     std::istream_iterator<std::string>{ilineit},
                     std::istream_iterator<std::string>{});
 
+            auto num_list_int = std::vector<int>();
             for (std::string c :num_list) {
-                std::cout << c << " ";
+                num_list_int.push_back(std::stoi(c));
             }
-            std::cout << std::endl;
+
+            count1 += line_difference_minmax(num_list_int);
+            count2 += evenly_divisible_value(num_list_int);
+
         }
+
+        std::cout << count1 << std::endl;
+        std::cout << count2 << std::endl;
     }
 
 }
