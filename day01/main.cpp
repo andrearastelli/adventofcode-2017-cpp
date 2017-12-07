9df5e6efce024512119ff562c71b3234d50ab07c
Andrea Rastelli <rastelliandrea87@gmail.com>
2017-12-07 19:34:48 +0000

    Day 01 - part 02

--- day01/main.cpp
+++ day01/main.cpp
@@ -2,27 +2,33 @@
 #include <fstream>
 #include <string>
 
-auto inverse_captcha(const std::string line) {
+
+/**
+ * Generates the inverse captcha function.
+ *
+ * From an input file with a single line string that contains numbers,
+ * this captcha checks the numeric value of each character with the next one
+ * and if they are equal,
+ *
+ * @param line numeric string line to parse
+ * @return the sum resulting value
+ */
+auto inverse_captcha(const std::string line, std::size_t step) {
 
     auto counter = 0;
 
     std::cout << "Line length: " << line.length() << std::endl;
 
-    for(std::size_t i=0; i<line.length()-1; ++i) {
+    for(std::size_t i=0; i<line.length(); ++i) {
         auto c1 = line.substr(i, 1);
-        auto c2 = line.substr(i + 1, 1);
+        std::size_t idx2 = (i + step) % line.length();
+        auto c2 = line.substr(idx2, 1);
         auto i1 = std::stoi(c1);
         auto i2 = std::stoi(c2);
 
         if (i1 == i2) {
             counter += i1;
         }
-    }
-
-    auto f = line.substr(0, 1);
-    auto l = line.substr(line.length()-1, 1);
-    if (std::stoi(f) == std::stoi(l)) {
-        counter += std::stoi(f);
     }
 
     return counter;
@@ -34,12 +40,12 @@ int main() {
 
     std::string line;
 
-    std::cout << inverse_captcha("1111") << std::endl;
-
     std::ifstream input_file("day01/input.txt");
     if (input_file.is_open()) {
         while(getline(input_file, line)) {
-            std::cout << inverse_captcha(line) << std::endl;
+            std::cout << inverse_captcha(line, 1) << std::endl;
+            std::size_t halfway = static_cast<std::size_t >(line.length() / 2);
+            std::cout << inverse_captcha(line, halfway) << std::endl;
         }
         input_file.close();
     }
