30a778a6181b46451816e19815825487e6c40dc0
Andrea Rastelli <rastelliandrea87@gmail.com>
2017-12-07 20:32:52 +0000

    input for day2 and first implementation

--- day02/main.cpp
+++ day02/main.cpp
@@ -1,6 +1,39 @@
 #include <iostream>
+#include <fstream>
+#include <vector>
+#include <string>
+#include <sstream>
+#include <algorithm>
+
+
+auto line_difference_minmax(std::vector<std::string> line) {
+    std::min(line);
+    std::max(line);
+}
+
+
+auto spreadsheet_checksum() {
+    return "";
+}
 
 
 int main() {
+
+    auto line = std::string();
+    auto input_file = std::ifstream("day02/input.txt");
+
+    if (input_file.is_open()) {
+        while(getline(input_file, line)) {
+            auto ilineit = std::istringstream(line);
+            auto num_list = std::vector<std::string>(
+                    std::istream_iterator<std::string>{ilineit},
+                    std::istream_iterator<std::string>{});
+
+            for (std::string c :num_list) {
+                std::cout << c << " ";
+            }
+            std::cout << std::endl;
+        }
+    }
 
 }
