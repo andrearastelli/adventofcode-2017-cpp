61f04b8935560fcde95b3f713ca1256bfbd607ff
Andrea Rastelli <rastelliandrea87@gmail.com>
2017-12-12 22:58:58 +0000

    Day 4 - Part 1

--- day04/main.cpp
+++ day04/main.cpp
@@ -1,12 +1,33 @@
 #include <iostream>
 #include <fstream>
+#include <sstream>
 #include <string>
-#include <list>
+#include <map>
+#include <vector>
+#include <cassert>
 
 
-auto check_passphrase_duplicate(std::list<std::string> passphrase_list) {
+auto check_passphrase_duplicate(std::vector<std::string> passphrase_list) {
+
+    auto passphrase_map = std::map<std::string, int>();
+
+    for (const std::string item:passphrase_list) {
+        ++passphrase_map[item];
+
+        if (passphrase_map[item] > 1) {
+            return false;
+        }
+    }
+
+    return true;
+}
 
 
+auto check_passphrase_rearranged(std::vector<std::string> passphrase_list) {
+
+    for (const std::string item : passphrase_list) {
+
+    }
 
     return false;
 }
@@ -14,16 +35,37 @@ auto check_passphrase_duplicate(std::list<std::string> passphrase_list) {
 
 int main() {
 
+    assert(check_passphrase_duplicate({"aa", "bb", "cc", "dd"}));
+    assert(!check_passphrase_duplicate({"aa", "bb", "cc", "dd", "aa"}));
+    assert(check_passphrase_duplicate({"aa", "bb", "cc", "dd", "aaa"}));
+
+    // assert(check_passphrase_rearranged({"abcde", "fghij"}));
+
     auto line = std::string();
     auto in_file = std::ifstream("day04/input.txt");
+
+    auto count_valid = 0;
+    auto count_rearranged = 0;
 
     if (in_file.is_open())
         while(getline(in_file, line)) {
 
-            std::cout << line << std::endl;
+            auto token = std::string();
+            auto token_stream = std::istringstream(line);
+            auto tokens = std::vector<std::string>();
+
+            while(std::getline(token_stream, token, ' ')) {
+                tokens.push_back(token);
+            }
+
+            count_valid += check_passphrase_duplicate(tokens);
+            count_rearranged += check_passphrase_rearranged(tokens);
 
         }
 
     in_file.close();
+
+    std::cout << count_valid << std::endl;
+    std::cout << count_rearranged << std::endl;
 
 }
